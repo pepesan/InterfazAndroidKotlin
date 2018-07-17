2c087bc6c34880d4993a4cc90154fe1444e6315e
David Vaquero <pepesan@gmail.com>
2018-07-17 11:32:09 +0200

    creada Activity base MiActivity para manejar el menú en común ,nueva activity con navegación

--- app/src/main/java/com/cursosdedesarrollo/interfazandroidkotlin/LayoutsActivity.kt
+++ app/src/main/java/com/cursosdedesarrollo/interfazandroidkotlin/LayoutsActivity.kt
@@ -6,18 +6,22 @@ import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_layouts.*
 
-class MainActivity : MiActivity() {
+class LayoutsActivity : MiActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_layouts)
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
+
+
+
 }
---
+++ app/src/main/java/com/cursosdedesarrollo/interfazandroidkotlin/MiActivity.kt
@@ -0,0 +1,34 @@
+package com.cursosdedesarrollo.interfazandroidkotlin
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+open class MiActivity : AppCompatActivity() {
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_layouts -> {
+                var intent = Intent(this,LayoutsActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
