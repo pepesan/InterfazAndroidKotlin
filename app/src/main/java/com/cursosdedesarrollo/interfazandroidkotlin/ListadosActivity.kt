c3fff2bf52a0e1a1a65b866517c629b6c778065f
David Vaquero <pepesan@gmail.com>
2018-07-17 13:26:04 +0200

    mejoras del listado para que sea dinámico

--- app/src/main/java/com/cursosdedesarrollo/interfazandroidkotlin/ListadosActivity.kt
+++ app/src/main/java/com/cursosdedesarrollo/interfazandroidkotlin/ListadosActivity.kt
@@ -3,17 +3,22 @@ package com.cursosdedesarrollo.interfazandroidkotlin
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_listados.*
+import kotlinx.android.synthetic.main.content_formulario.*
 import kotlinx.android.synthetic.main.content_listados.*
 
 class ListadosActivity : AppCompatActivity() {
 
-    private val datos = arrayOf("uno", "dos")
+    private val datos = mutableListOf("uno", "dos")
+
+    private lateinit var adapter: ArrayAdapter<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,13 +32,34 @@ class ListadosActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         texto.setVisibility(View.GONE)
         listado.visibility=View.VISIBLE
-        val adapter = ArrayAdapter<String>(this, R.layout.item, datos)
+        adapter = ArrayAdapter<String>(this, R.layout.item, datos)
         listado.setTextFilterEnabled(true)
         listado.setAdapter(adapter)
         listado.setOnItemClickListener(AdapterView.OnItemClickListener {
             parent, view, position, id ->
             Toast.makeText(this@ListadosActivity, datos[position], Toast.LENGTH_LONG).show()
+            datos.removeAt(position)
+            adapter.notifyDataSetChanged()
         })
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.listado_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_add_listado -> {
+                datos.add("pepe")
+                adapter.notifyDataSetChanged()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 }
